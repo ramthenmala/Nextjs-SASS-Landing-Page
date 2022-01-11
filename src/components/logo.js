10906ff3ab44a70646569408f780598a36c74ce9
Ram kumar <webbieram@outlook.com>
2022-01-11 23:13:10 +0530

    initial commit

---
+++ src/components/logo.js
@@ -0,0 +1,20 @@
+/** @jsx jsx */
+import { jsx, Image } from "theme-ui";
+import { Link } from "components/link";
+
+export default function Logo({ src, ...rest }) {
+  return (
+    <Link
+      path="/"
+      sx={{
+        variant: "links.logo",
+        display: "flex",
+        mr: 15,
+        cursor: "pointer",
+      }}
+      {...rest}
+    >
+      <Image src={src} alt="Website Logo" />
+    </Link>
+  );
+}
